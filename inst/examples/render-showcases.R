9b1a91df22c3e9b639b36b310943c7498a42098f
Yousa-Mirage <Yousa-Mirage@foxmail.com>
2026-03-08 20:23:59 +0800

    chore: don't generate the patched example plot

--- inst/examples/render-showcases.R
+++ inst/examples/render-showcases.R
@@ -1,8 +1,3 @@
-suppressPackageStartupMessages({
-  library(ggplot2)
-  library(patchwork)
-})
-
 if (!requireNamespace("ragg", quietly = TRUE)) {
   stop("Package 'ragg' is required to render showcase PNG files.", call. = FALSE)
 }
@@ -12,30 +7,10 @@ source("inst/examples/geom-showcase.R", local = TRUE)
 source("inst/examples/element-showcase.R", local = TRUE)
 
 showcase_dimensions <- list(
-  panel_width = 10,
-  panel_height = 6,
-  combined_width = 10,
-  combined_height = 13.75,
+  width = 10,
+  height = 6,
   dpi = 320
 )
-
-build_showcase_plots <- function() {
-  list(
-    annotate = build_annotate_showcase(),
-    geom = build_geom_showcase(),
-    element = build_element_showcase()
-  )
-}
-
-build_combined_showcase <- function(plots) {
-  patchwork::wrap_plots(
-    plots$annotate,
-    plots$geom,
-    plots$element,
-    ncol = 1,
-    guides = "keep"
-  )
-}
 
 save_showcase_png <- function(plot, output, width, height, dpi) {
   elapsed <- system.time({
@@ -56,32 +31,31 @@ save_showcase_png <- function(plot, output, width, height, dpi) {
   invisible(output)
 }
 
-render_showcase_panels <- function(
+render_all_showcases <- function(
   output_dir = "man/figures",
-  width = showcase_dimensions$panel_width,
-  height = showcase_dimensions$panel_height,
-  dpi = showcase_dimensions$dpi,
-  plots = build_showcase_plots()
+  width = showcase_dimensions$width,
+  height = showcase_dimensions$height,
+  dpi = showcase_dimensions$dpi
 ) {
   dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 
   outputs <- list(
     annotate = save_showcase_png(
-      plots$annotate,
+      build_annotate_showcase(),
       file.path(output_dir, "annotate-showcase.png"),
       width,
       height,
       dpi
     ),
     geom = save_showcase_png(
-      plots$geom,
+      build_geom_showcase(),
       file.path(output_dir, "geom-showcase.png"),
       width,
       height,
       dpi
     ),
     element = save_showcase_png(
-      plots$element,
+      build_element_showcase(),
       file.path(output_dir, "element-showcase.png"),
       width,
       height,
@@ -90,46 +64,6 @@ render_showcase_panels <- function(
   )
 
   invisible(outputs)
-}
-
-render_combined_showcase <- function(
-  output = "man/figures/ggtypst-showcase.png",
-  width = showcase_dimensions$combined_width,
-  height = showcase_dimensions$combined_height,
-  dpi = showcase_dimensions$dpi,
-  plots = build_showcase_plots()
-) {
-  dir.create(dirname(output), recursive = TRUE, showWarnings = FALSE)
-  save_showcase_png(build_combined_showcase(plots), output, width, height, dpi)
-}
-
-render_all_showcases <- function(
-  output_dir = "man/figures",
-  panel_width = showcase_dimensions$panel_width,
-  panel_height = showcase_dimensions$panel_height,
-  combined_width = showcase_dimensions$combined_width,
-  combined_height = showcase_dimensions$combined_height,
-  dpi = showcase_dimensions$dpi
-) {
-  plots <- build_showcase_plots()
-
-  panel_outputs <- render_showcase_panels(
-    output_dir = output_dir,
-    width = panel_width,
-    height = panel_height,
-    dpi = dpi,
-    plots = plots
-  )
-
-  combined_output <- render_combined_showcase(
-    output = file.path(output_dir, "ggtypst-showcase.png"),
-    width = combined_width,
-    height = combined_height,
-    dpi = dpi,
-    plots = plots
-  )
-
-  invisible(c(panel_outputs, combined = combined_output))
 }
 
 if (sys.nframe() == 0) {
